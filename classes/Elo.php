6a8b69e380f06871ef04b81aa06832ab6e6003f4
Frank Hoppe <hoppe.berlin@googlemail.com>
2017-06-25 21:05:20 +0200

    6-fach vorhandenen Code zur Elo-Abfrage in eine Funktion überführt

--- classes/Elo.php
+++ classes/Elo.php
@@ -32,6 +32,7 @@ class Elo extends \Module
 	 * @var string
 	 */
 	protected $strTemplate = 'mod_elo';
+	var $cache = false;
 
 	/**
 	 * Display a wildcard in the back end
@@ -59,276 +60,100 @@ class Elo extends \Module
 	 */
 	protected function compile()
 	{
-		$this->import('Database');
-
 		$cachetime = 3600 * 24 * 40; // 40 Tage
 
 		// Aktuelle Liste ermitteln
-		$objActiv = $this->Database->prepare('SELECT * FROM tl_elo_listen WHERE published=? ORDER BY datum DESC')
-		                           ->limit(1)
-		                           ->execute(1);
+		$objActiv = \Database::getInstance()->prepare('SELECT * FROM tl_elo_listen WHERE published=? ORDER BY datum DESC')
+		                                    ->limit(1)
+		                                    ->execute(1);
 
 		// Cache initialisieren
-		$cache = new \Samson\Helper\Cache('Elo');
-		$cache->eraseExpired(); // Cache aufräumen, abgelaufene Schlüssel löschen
+		$this->cache = new \Samson\Helper\Cache('Elo');
+		$this->cache->eraseExpired(); // Cache aufräumen, abgelaufene Schlüssel löschen
 
 		$this->Template->headline = 'FIDE-Ratingliste Deutschland';
 		$this->Template->hl = 'h2';
 		$this->Template->datum = date('d.m.Y', $objActiv->datum).' ('.$objActiv->title.')';
 
-		/*******************************************************
-		 *
-		 * Elo-Liste klassisch laden
-		 *
-		 ******************************************************/
-		$cachekey = 'EloN_'.$this->elo_topcount.'_'.$objActiv->id;
-		if($cache->isCached($cachekey))
-		{
-			// Daten aus dem Cache laden
-			$eloN = $cache->retrieve($cachekey);
-		}
-		else
-		{
-			// Daten aus der Datenbank laden
-			$objEloN = $this->Database->prepare('SELECT * FROM tl_elo WHERE pid=? AND published=? AND flag NOT LIKE ? ORDER BY rating DESC')
-			                           ->limit($this->elo_topcount)
-			                           ->execute($objActiv->id, 1, '%i%');
-
-			// Elo klassisch zuweisen
-			if($objEloN->numRows > 1)
-			{
-				$eloN = array();
-				// Datensätze anzeigen
-				while($objEloN->next()) 
-				{
-					$line = $objEloN->intent;
-					$line .= ($line) ? ' '.$objEloN->prename : $objEloN->prename; 
-					$line .= ($line) ? ' '.$objEloN->surname : $objEloN->surname; 
-					$eloN[] = array
-					(
-						'name' 	=> $line,
-						'elo'  	=> $objEloN->rating,
-						'fid' 	=> $objEloN->fideid,
-						'title'	=> ($objEloN->title) ? $objEloN->title . ' ' : (($objEloN->w_title) ? $objEloN->w_title . ' ': ''),
-					);
-				}
-				// Daten im Cache speichern
-				$cache->store($cachekey, $eloN, $cachetime);
-			}
-		}
-
-
-		/*******************************************************
-		 *
-		 * Elo-Liste Blitzschach laden
-		 *
-		 ******************************************************/
-		$cachekey = 'EloB_'.$this->elo_topcount.'_'.$objActiv->id;
-		if($cache->isCached($cachekey))
-		{
-			// Daten aus dem Cache laden
-			$eloB = $cache->retrieve($cachekey);
-		}
-		else
-		{
-			// Daten aus der Datenbank laden
-			$objEloB = $this->Database->prepare('SELECT * FROM tl_elo WHERE pid=? AND published=? AND flag NOT LIKE ? ORDER BY blitz_rating DESC')
-			                          ->limit($this->elo_topcount)
-			                          ->execute($objActiv->id, 1, '%i%');
-			// Elo Blitzschach zuweisen
-			if($objEloB->numRows > 1)
-			{
-				$eloB = array();
-				// Datensätze anzeigen
-				while($objEloB->next()) 
-				{
-					$line = $objEloB->intent;
-					$line .= ($line) ? ' '.$objEloB->prename : $objEloB->prename; 
-					$line .= ($line) ? ' '.$objEloB->surname : $objEloB->surname; 
-					$eloB[] = array
-					(
-						'name' 	=> $line,
-						'elo'  	=> $objEloB->blitz_rating,
-						'fid' 	=> $objEloB->fideid,
-						'title'	=> ($objEloB->title) ? $objEloB->title . ' ' : (($objEloB->w_title) ? $objEloB->w_title . ' ': ''),
-					);
-				}
-				// Daten im Cache speichern
-				$cache->store($cachekey, $eloB, $cachetime);
-			}
-		}
-
-
-		/*******************************************************
-		 *
-		 * Elo-Liste Schnellschach laden
-		 *
-		 ******************************************************/
-		$cachekey = 'EloR_'.$this->elo_topcount.'_'.$objActiv->id;
-		if($cache->isCached($cachekey))
-		{
-			// Daten aus dem Cache laden
-			$eloR = $cache->retrieve($cachekey);
-		}
-		else
-		{
-			// Daten aus der Datenbank laden
-			$objEloR = $this->Database->prepare('SELECT * FROM tl_elo WHERE pid=? AND published=? AND flag NOT LIKE ? ORDER BY rapid_rating DESC')
-			                          ->limit($this->elo_topcount)
-			                          ->execute($objActiv->id, 1, '%i%');
-			// Elo Schnellschach zuweisen
-			if($objEloR->numRows > 1)
-			{
-				$eloR = array();
-				// Datensätze anzeigen
-				while($objEloR->next()) 
-				{
-					$line = $objEloR->intent;
-					$line .= ($line) ? ' '.$objEloR->prename : $objEloR->prename; 
-					$line .= ($line) ? ' '.$objEloR->surname : $objEloR->surname; 
-					$eloR[] = array
-					(
-						'name' 	=> $line,
-						'elo'  	=> $objEloR->rapid_rating,
-						'fid' 	=> $objEloR->fideid,
-						'title'	=> ($objEloR->title) ? $objEloR->title . ' ' : (($objEloR->w_title) ? $objEloR->w_title . ' ': ''),
-					);
-				}
-				// Daten im Cache speichern
-				$cache->store($cachekey, $eloR, $cachetime);
-			}
-		}
-
-
-		/*******************************************************
-		 *
-		 * Elo-Liste klassisch Frauen laden
-		 *
-		 ******************************************************/
-		$cachekey = 'EloNw_'.$this->elo_topcount.'_'.$objActiv->id;
-		if($cache->isCached($cachekey))
-		{
-			// Daten aus dem Cache laden
-			$eloNw = $cache->retrieve($cachekey);
-		}
-		else
-		{
-			// Daten aus der Datenbank laden
-			$objEloNw = $this->Database->prepare('SELECT * FROM tl_elo WHERE pid=? AND published=? AND flag NOT LIKE ? AND sex=? ORDER BY rating DESC')
-			                           ->limit($this->elo_topcount)
-			                           ->execute($objActiv->id, 1, '%i%', 'F');
-			// Elo klassisch zuweisen
-			if($objEloNw->numRows > 1)
-			{
-				$eloNw = array();
-				// Datensätze anzeigen
-				while($objEloNw->next()) 
-				{
-					$line = $objEloNw->intent;
-					$line .= ($line) ? ' '.$objEloNw->prename : $objEloNw->prename; 
-					$line .= ($line) ? ' '.$objEloNw->surname : $objEloNw->surname; 
-					$eloNw[] = array
-					(
-						'name' 	=> $line,
-						'elo'  	=> $objEloNw->rating,
-						'fid' 	=> $objEloNw->fideid,
-						'title'	=> ($objEloNw->title) ? $objEloNw->title . ' ' : (($objEloNw->w_title) ? $objEloNw->w_title . ' ': ''),
-					);
-				}
-				// Daten im Cache speichern
-				$cache->store($cachekey, $eloNw, $cachetime);
-			}
-		}
-
-
-		/*******************************************************
-		 *
-		 * Elo-Liste Blitzschach Frauen laden
-		 *
-		 ******************************************************/
-		$cachekey = 'EloBw_'.$this->elo_topcount.'_'.$objActiv->id;
-		if($cache->isCached($cachekey))
-		{
-			// Daten aus dem Cache laden
-			$eloBw = $cache->retrieve($cachekey);
-		}
-		else
-		{
-			// Daten aus der Datenbank laden
-			$objEloBw = $this->Database->prepare('SELECT * FROM tl_elo WHERE pid=? AND published=? AND flag NOT LIKE ? AND sex=? ORDER BY blitz_rating DESC')
-			                           ->limit($this->elo_topcount)
-			                           ->execute($objActiv->id, 1, '%i%', 'F');
-			// Elo Blitzschach zuweisen
-			if($objEloBw->numRows > 1)
-			{
-				$eloBw = array();
-				// Datensätze anzeigen
-				while($objEloBw->next()) 
-				{
-					$line = $objEloBw->intent;
-					$line .= ($line) ? ' '.$objEloBw->prename : $objEloBw->prename; 
-					$line .= ($line) ? ' '.$objEloBw->surname : $objEloBw->surname; 
-					$eloBw[] = array
-					(
-						'name' 	=> $line,
-						'elo'  	=> $objEloBw->blitz_rating,
-						'fid' 	=> $objEloBw->fideid,
-						'title'	=> ($objEloBw->title) ? $objEloBw->title . ' ' : (($objEloBw->w_title) ? $objEloBw->w_title . ' ': ''),
-					);
-				}
-				// Daten im Cache speichern
-				$cache->store($cachekey, $eloBw, $cachetime);
-			}
-		}
-
-
-		/*******************************************************
-		 *
-		 * Elo-Liste Schnellschach Frauen laden
-		 *
-		 ******************************************************/
-		$cachekey = 'EloRw_'.$this->elo_topcount.'_'.$objActiv->id;
-		if($cache->isCached($cachekey))
-		{
-			// Daten aus dem Cache laden
-			$eloBw = $cache->retrieve($cachekey);
-		}
-		else
-		{
-			// Daten aus der Datenbank laden
-			$objEloRw = $this->Database->prepare('SELECT * FROM tl_elo WHERE pid=? AND published=? AND flag NOT LIKE ? AND sex=? ORDER BY rapid_rating DESC')
-			                           ->limit($this->elo_topcount)
-			                           ->execute($objActiv->id, 1, '%i%', 'F');
-			// Elo Schnellschach zuweisen
-			if($objEloRw->numRows > 1)
-			{
-				$eloRw = array();
-				// Datensätze anzeigen
-				while($objEloRw->next()) 
-				{
-					$line = $objEloRw->intent;
-					$line .= ($line) ? ' '.$objEloRw->prename : $objEloRw->prename; 
-					$line .= ($line) ? ' '.$objEloRw->surname : $objEloRw->surname; 
-					$eloRw[] = array
-					(
-						'name' 	=> $line,
-						'elo'  	=> $objEloRw->rapid_rating,
-						'fid' 	=> $objEloRw->fideid,
-						'title'	=> ($objEloRw->title) ? $objEloRw->title . ' ' : (($objEloRw->w_title) ? $objEloRw->w_title . ' ': ''),
-					);
-				}
-				// Daten im Cache speichern
-				$cache->store($cachekey, $eloRw, $cachetime);
-			}
-		}
-
 		$this->Template->count = $this->elo_topcount;
-		$this->Template->eloN = $eloN;
-		$this->Template->eloB = $eloB;
-		$this->Template->eloR = $eloR;
-		$this->Template->eloNw = $eloNw;
-		$this->Template->eloBw = $eloBw;
-		$this->Template->eloRw = $eloRw;
+		$this->Template->eloN = $this->getEloliste($objActiv->id, 'eloN', $this->elo_topcount);
+		$this->Template->eloB = $this->getEloliste($objActiv->id, 'eloB', $this->elo_topcount);
+		$this->Template->eloR = $this->getEloliste($objActiv->id, 'eloR', $this->elo_topcount);
+		$this->Template->eloNw = $this->getEloliste($objActiv->id, 'eloNw', $this->elo_topcount);
+		$this->Template->eloBw = $this->getEloliste($objActiv->id, 'eloBw', $this->elo_topcount);
+		$this->Template->eloRw = $this->getEloliste($objActiv->id, 'eloRw', $this->elo_topcount);
 
+	}
+
+	/**
+	 * Eloliste aus Datenbank laden
+	 * @param integer $listid         ID der Eloliste
+	 * @param string $listtyp         Typ der Eloliste
+	 * @param integer $count          Anzahl der Topeinträge der Eloliste
+	 * @return array                  Gefundene Datensätze
+	 */
+	function getEloliste($listid, $listtype, $count)
+	{
+
+		switch($listtype)
+		{
+			case 'eloN':
+				$sql = 'ORDER BY rating DESC';
+				break;
+			case 'eloB':
+				$sql = 'ORDER BY blitz_rating DESC';
+				break;
+			case 'eloR':
+				$sql = 'ORDER BY rapid_rating DESC';
+				break;
+			case 'eloNw':
+				$sql = 'AND sex=\'F\' ORDER BY rating DESC';
+				break;
+			case 'eloBw':
+				$sql = 'AND sex=\'F\' ORDER BY blitz_rating DESC';
+				break;
+			case 'eloRw':
+				$sql = 'AND sex=\'F\' ORDER BY rapid_rating DESC';
+				break;
+			default:
+		}
+
+		$cachekey = $listtype.'_'.$count.'_'.$listid;
+
+		if($this->cache->isCached($cachekey))
+		{
+			// Daten aus dem Cache laden
+			$result = $this->cache->retrieve($cachekey);
+		}
+		else
+		{
+			// Daten aus der Datenbank laden
+			$objElo = \Database::getInstance()->prepare('SELECT * FROM tl_elo WHERE pid=? AND published=? AND flag NOT LIKE ? '.$sql)
+			                                  ->limit($count)
+			                                  ->execute($listid, 1, '%i%');
+			// Elo zuweisen
+			if($objElo->numRows > 1)
+			{
+				$result = array();
+				// Datensätze anzeigen
+				while($objElo->next()) 
+				{
+					$line = $objElo->intent;
+					$line .= ($line) ? ' '.$objElo->prename : $objElo->prename; 
+					$line .= ($line) ? ' '.$objElo->surname : $objElo->surname; 
+					$result[] = array
+					(
+						'name' 	=> $line,
+						'elo'  	=> (substr($listtype,0,4) == 'eloN') ? $objElo->rating : ((substr($listtype,0,4) == 'eloB') ? $objElo->blitz_rating : $objElo->rapid_rating),
+						'fid' 	=> $objElo->fideid,
+						'title'	=> ($objElo->title) ? $objElo->title . ' ' : (($objElo->w_title) ? $objElo->w_title . ' ': ''),
+					);
+				}
+				// Daten im Cache speichern
+				$this->cache->store($cachekey, $result, $cachetime);
+			}
+		}
+
+		return $result;
 	}
 }
